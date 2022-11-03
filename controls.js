a53609e197a906cb4ea64a6de622fd9d7863a2d9
hpharis258 <60554302+hpharis258@users.noreply.github.com>
2022-11-03 23:59:30 +0000

    fix bullet collisions

--- controls.js
+++
@@ -1,6 +0,0 @@
-addEventListener("keydown", function(e){
-    if(e.code = "KeyW")
-    {
-        
-    }
-})
--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-// Player Class Import
-import {Player} from "./player.js";
-// Spawn Player
-const player = new PLayer(100, 100, 20, 20);
-player.draw();
+let player;
+let enemies = [];
+function setup(){
+    createCanvas(700,700);
+    player = new Player();
+}
+
+function draw(){
+    background(100,100,100);
+    rectMode(CENTER);
+    player.draw();
+    player.update();
+    //
+    for(let i = enemies.length - 1; i >= 0; i--)
+    {
+        enemies[i].draw();
+        enemies[i].update();
+        //
+        if(player.hasShot(enemies[i]))
+        {
+            enemies.splice(i, 1);
+        }
+    }
+    if(frameCount % 200 == 0){
+        enemies.push(new Enemy(2));
+    }
+}
+//
+function mouseClicked()
+{
+    player.shoot();
+}
